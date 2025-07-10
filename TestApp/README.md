c8bb62676975311f9e6433e0ed0761584da09b06
Mark <mark@markjacobsen.net>
2025-07-10 09:20:14 -0400

    TestApp

---
+++ TestApp/README.md
@@ -0,0 +1,2 @@
+A test application to try out changes/updates to libraries
+
---
+++ TestApp/src/TestApp.cs
@@ -0,0 +1,17 @@
+﻿namespace CFG2.Test;
+
+using CFG2.Utils.AppLib;
+
+public class TestApp
+{
+    static void Main(string[] args)
+    {
+        // This is a test application to demonstrate the use of AppLib
+        Console.WriteLine("Welcome to the Test Application using AppLib!");
+
+        // Initialize AppLib
+        AppLib app = new AppLib("TestApp");
+
+        Console.WriteLine("Test Application completed successfully.");
+    }
+}
